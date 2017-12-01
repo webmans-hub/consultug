7ec8a8f345e36733c5bf6953b3557c21445f18b0
Evgeniy Kirichenko <kirichenko_ev@mail.com>
2017-12-01 17:44:33 +0300

    Языковой файл фотобитвы

---
+++ system/languages/ru/controllers/photobattle/photobattle.php
@@ -0,0 +1,54 @@
+<?php
+
+    define('LANG_PHOTOBATTLE_CONTROLLER', 'Фотобитвы');
+	
+    define('LANG_PHOTOBATTLE_ADD', 'Создать битву');
+    define('LANG_PHOTOBATTLE_EDIT', 'Редактировать битву');
+    define('LANG_PHOTOBATTLE_DELETE', 'Удалить битву');
+    define('LANG_PHOTOBATTLE_START', 'Запустить битву');
+    define('LANG_PHOTOBATTLE_STOP', 'Завершить битву');
+	define('LANG_PHOTOBATTLE_PHOTO_DELETE', 'Удалить фотографию');
+    
+	define('LANG_PHOTOBATTLE_TITLE', 'Название битвы');
+	define('LANG_PHOTOBATTLE_DATE_END', 'Дата окончания битвы');
+	define('LANG_PHOTOBATTLE_MIN_USERS', 'Количество участников для автозапуска битвы');
+	define('LANG_PHOTOBATTLE_STATUS', 'Статус битвы');
+	define('LANG_PHOTOBATTLE_STATUS_OPENED', 'Запущена');
+	define('LANG_PHOTOBATTLE_STATUS_PENDING', 'Набор участников');
+	define('LANG_PHOTOBATTLE_STATUS_MODERATION', 'Проверка перед запуском');
+	define('LANG_PHOTOBATTLE_STATUS_CLOSED', 'Завершена');
+	define('LANG_PHOTOBATTLE_LOGO', 'Логотип битвы');
+	
+	define('LANG_PHOTOBATTLE_NONE', 'Нет доступных битв');
+	define('LANG_PHOTOBATTLE_SPELLCOUNT', 'участник|участника|участников');
+	
+	define('LANG_PHOTOBATTLE_JOIN', 'Принять участие');	
+	define('LANG_PHOTOBATTLE_JOIN_TITLE', 'Название фотографии');
+	define('LANG_PHOTOBATTLE_JOIN_PHOTO', 'Файл фотографии');
+	
+	define('LANG_PHOTOBATTLE_SELECT_VS', 'Выберите лучшую из двух фотографий');
+	define('LANG_PHOTOBATTLE_ZOOM', 'Увеличить');
+	define('LANG_PHOTOBATTLE_WINS', 'Побед');
+	
+	define('LANG_PHOTOBATTLE_1_PLACE', 'I место');
+	define('LANG_PHOTOBATTLE_2_PLACE', 'II место');
+	define('LANG_PHOTOBATTLE_3_PLACE', 'III место');
+	
+	define('LANG_PHOTOBATTLE_WINNERS', 'Победители битвы');
+	define('LANG_PHOTOBATTLE_OTHERS', 'Остальные участники');
+	define('LANG_PHOTOBATTLE_YOU_VOTED', 'Вы уже оценили все фотографии');
+	
+	define('LANG_PHOTOBATTLE_MODERATION_NOTICE', 'Фотобитва "%s" требует проверки перед запуском');
+	
+	define('LANG_PHOTOBATTLE_OPTIONS_PERPAGE', 'Битв на странице');
+	define('LANG_PHOTOBATTLE_OPTIONS_SORT', 'Сортировать битвы');
+	define('LANG_PHOTOBATTLE_OPTIONS_SORT_DATE', 'По дате создания');
+	define('LANG_PHOTOBATTLE_OPTIONS_SORT_STATUS', 'По статусу');
+	define('LANG_PHOTOBATTLE_OPTIONS_SHOW_NAMES', 'Показывать имена участников');
+	define('LANG_PHOTOBATTLE_OPTIONS_ADMIN_ID', 'Ответственный администратор');
+	define('LANG_PHOTOBATTLE_OPTIONS_ADMIN_ID_HINT', 'Указанный администратор будет получать уведомления о битвах требующих проверки');
+	
+	define('LANG_RULE_PHOTOBATTLE_ADD', 'Создание битв');
+	define('LANG_RULE_PHOTOBATTLE_EDIT', 'Управление битвами');
+	define('LANG_RULE_PHOTOBATTLE_JOIN', 'Участие в битвах');
+	define('LANG_RULE_PHOTOBATTLE_MAX_PHOTOS', 'Максимальное число фото в битве');
